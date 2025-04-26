765eee6d44faa847e1d70f1e68e8397144fc1119
kavitharanitamalapakula <kavitharaninissy@gmail.com>
2025-04-27 02:28:24 +0530

    updated

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import { useTheme } from "./ThemeContext";
 
 const Navbar = () => {
-  const { theme, toggleTheme } = useTheme();
+    const { theme, toggleTheme } = useTheme();
 
-  return (
-    <nav className={theme === "light" ? "navbar-light" : "navbar-dark"}>
-      <h1>My Website</h1>
-      <button onClick={toggleTheme}>
-        {theme === "light" ? "🌙" : "🌟"}
-      </button>
-    </nav>
-  );
+    return (
+        <nav className={theme === "light" ? "navbar-light" : "navbar-dark"}>
+            <h1>My Website</h1>
+            <button onClick={toggleTheme} style={{ border: "none" }}>
+                {theme === "light" ? "🌙" : "🌟"}
+            </button>
+        </nav>
+    );
 };
 
 export default Navbar;
